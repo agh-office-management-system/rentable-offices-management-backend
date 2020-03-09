871117e809221fe2eff73a184e3ce6c598f0c33e
Tomasz Odrzywolek <tomeko92@gmail.com>
2020-03-09 23:24:53 +0100

    add spring boot dependencies

---
+++ src/main/java/pl/edu/agh/rentableoffices/RentableOfficesApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.rentableoffices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentableOfficesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentableOfficesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/agh/rentableoffices/RentableOfficesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.rentableoffices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentableOfficesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
